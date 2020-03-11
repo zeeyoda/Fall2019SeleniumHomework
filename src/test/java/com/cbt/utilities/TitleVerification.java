07ce14640df3d944769e8ab91755d59c325f79db
zeeyoda <ziyoda2000@yahoo.com>
2020-03-10 23:13:57 -0400

    updated H/W

--- src/test/java/com/cbt/utilities/TitleVerification.java
+++ src/test/java/com/cbt/utilities/TitleVerification.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class TitleVerification {
     static WebDriver driver;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         List<String> urls = Arrays.asList("http://practice.cybertekschool.com/",
                 "http://practice.cybertekschool.com/dropdown",
                 "http://practice.cybertekschool.com/login");
@@ -27,9 +27,9 @@ public class TitleVerification {
         }
 
 
-        for (int i = 0; i < urlsTitles.size(); i++) {
+        for (int i = 0; i < urlsTitles.size()-1; i++) {
             StringUtility.verifyEquals(urlsTitles.get(i), urlsTitles.get(i + 1));
-
+            Thread.sleep(3000);
         }
         driver.close();
 
